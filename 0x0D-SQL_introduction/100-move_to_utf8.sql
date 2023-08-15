7bcd258f834de450bebc64f5e5f639d3573069bc
raocindy <cindyisogie@gmail.com>
2023-08-15 12:14:03 -0700

    changes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
--- List all records of 'second_table' of db 'hbtn_0c_0'
-SELECT score, name
-FROM second_table
-HAVING name IS NOT NULL
-ORDER BY score DESC;
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
